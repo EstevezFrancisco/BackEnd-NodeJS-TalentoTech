377eb2dea963d8b9d74775db675a48c646f9a9a3
Francisco Estevez <franciscoestevez987@gmail.com>
2025-12-11 22:33:40 -0300

    agrega error 404

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ const app = express();
 app.use(cors());
 
 
+
+
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not found" });
+});
+
 const PORT = 3000;
 server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
